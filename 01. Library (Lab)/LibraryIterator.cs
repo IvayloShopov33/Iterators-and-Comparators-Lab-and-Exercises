0a50eb9fd81a7e302d8262e08e108019e3d52e5c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-01 01:08:42 +0200

    Add files via upload

---
+++ 01. Library (Lab)/LibraryIterator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int index;
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            this.index = -1;
+        }
+
+        public Book Current => this.books[index];
+
+        object IEnumerator.Current => this.Current;
+
+        public bool MoveNext()
+        {
+            this.index++;
+            return this.index < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ 01. Library (Lab)/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (Book book in libraryTwo)
+            {
+                Console.Write($"Name: {book.Title}, Published in: {book.Year}, ");
+                if (book.Authors.Count == 0)
+                {
+                    Console.WriteLine("Anonymous author(s)");
+                }
+                else if (book.Authors.Count == 1)
+                {
+                    Console.WriteLine($"Author: {string.Join(" ", book.Authors)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Authors: {string.Join(", ", book.Authors)}");
+                }
+            }
+        }
+    }
+}
