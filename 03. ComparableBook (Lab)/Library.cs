d13a862e08fd3ecd3e7f69283d5224d013da7118
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-02 01:55:50 +0200

    Add files via upload

---
+++ 03. ComparableBook (Lab)/Library.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = books.OrderBy(x => x.Year).ThenBy(x => x.Title).ToList();
+            }
+
+            public Book Current => this.books[this.index];
+
+            object IEnumerator.Current => this.Current;
+
+
+            public bool MoveNext()
+            {
+                this.index++;
+                return this.index >= 0 && this.index < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.index = -1;
+            }
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
