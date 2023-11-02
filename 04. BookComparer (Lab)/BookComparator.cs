a3a3253f4c473636749cf9b05accf1395945970a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-03 00:27:50 +0200

    Add files via upload

---
+++ 04. BookComparer (Lab)/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book book1, Book book2)
+        {
+            if (book1.Title.CompareTo(book2.Title) > 0)
+            {
+                return 1;
+            }
+            else if (book1.Title.CompareTo(book2.Title) < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return book2.Year.CompareTo(book1.Year);
+            }
+        }
+    }
+}
---
+++ 04. BookComparer (Lab)/Library.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books.OrderBy(x => x, new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int index;
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.Reset();
+            this.books = books;
+        }
+
+        public Book Current => this.books[this.index];
+
+        object IEnumerator.Current => this.Current;
+
+
+        public bool MoveNext()
+        {
+            this.index++;
+            return this.index >= 0 && this.index < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
