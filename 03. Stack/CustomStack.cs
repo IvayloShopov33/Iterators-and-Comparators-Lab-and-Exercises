2500a9514b1357fb3f6f059674768cbe71d3f3c7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-05 01:35:05 +0200

    Add files via upload

---
+++ 03. Stack/CustomStack.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] items = new T[InitialCapacity];
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            if (this.items.Length == this.Count)
+            {
+                this.Resize();
+            }
+
+            this.items[Count] = item;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T removedItem = this.items[this.Count - 1];
+            this.Count--;
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+
+            return removedItem;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void Shrink()
+        {
+            T[] copy = new T[this.items.Length / 4];
+            for (int i = 0; i < copy.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[this.items.Length * 2];
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+    }
+}
---
+++ 03. Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> numbers = new();
+
+            string command;
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    if (numbers.Count > 0)
+                    {
+                        Console.WriteLine(string.Join(Environment.NewLine, numbers));
+                        Console.WriteLine(string.Join(Environment.NewLine, numbers));
+                    }
+
+                    break;
+                }
+
+                string[] commandDetails = command.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string action = commandDetails[0];
+
+                switch (action)
+                {
+                    case "Push":
+                        int[] itemsToPush = commandDetails.Skip(1).Select(int.Parse).ToArray();
+                        foreach (int item in itemsToPush)
+                        {
+                            numbers.Push(item);
+                        }
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            numbers.Pop();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
