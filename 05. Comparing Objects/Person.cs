1777109942ec3e12abd25e96676c3f66507b93f3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-06 01:59:42 +0200

    Add files via upload

---
+++ 05. Comparing Objects/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace Comparing_Objects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            result = this.Age.CompareTo(other.Age);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return this.Town.CompareTo(other.Town);
+        }
+    }
+}
---
+++ 05. Comparing Objects/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Comparing_Objects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new();
+
+            string input = string.Empty;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] personDetails = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personDetails[0];
+                int year = int.Parse(personDetails[1]);
+                string town = personDetails[2];
+
+                Person person = new(name, year, town);
+                people.Add(person);
+            }
+
+            int indexOfComparedPerson = int.Parse(Console.ReadLine());
+            Person personToCompare = people[indexOfComparedPerson - 1];
+
+            int equalCount = 0;
+            int differentCount = 0;
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(personToCompare) == 0)
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    differentCount++;
+                }
+            }
+
+            if (equalCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                differentCount = people.Count - equalCount;
+                Console.WriteLine($"{equalCount} {differentCount} {people.Count}");
+            }
+        }
+    }
+}
