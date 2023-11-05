cd207b1c80ea844cb69d47c2a843f22f19b5f242
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-06 01:59:11 +0200

    Add files via upload

---
+++ 04. Froggy/Lake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            for (int i = this.stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    yield return this.stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
