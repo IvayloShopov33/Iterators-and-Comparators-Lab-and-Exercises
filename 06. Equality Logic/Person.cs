86114bce05bc38de2ec4c9e491591fa8a1cea12d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-07 01:59:35 +0200

    Add files via upload

---
+++ 06. Equality Logic/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace Equality_Logic
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+
+            if (person == null)
+            {
+                return false;
+            }
+
+            return this.Name == person.Name && this.Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = this.Name.GetHashCode() + this.Age.GetHashCode();
+
+            return hashCode;
+        }
+    }
+}
---
+++ 06. Equality Logic/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Equality_Logic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> peopleHashSet = new();
+            SortedSet<Person> peopleSortedSet = new();
+
+            int peopleCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person()
+                {
+                    Name = personDetails[0],
+                    Age = int.Parse(personDetails[1])
+                };
+
+                peopleHashSet.Add(person);
+                peopleSortedSet.Add(person);
+            }
+
+            Console.WriteLine(peopleHashSet.Count);
+            Console.WriteLine(peopleSortedSet.Count);
+        }
+    }
+}
