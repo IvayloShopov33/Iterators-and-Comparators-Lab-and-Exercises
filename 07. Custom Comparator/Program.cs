50b0439999d9ab916ef8396c120e0c90b750d680
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-07 02:00:11 +0200

    Add files via upload

---
+++ 07. Custom Comparator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07._Custom_Comparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Func<int, int, int> compareNumbers = (x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return x.CompareTo(y);
+            };
+
+            Array.Sort(numbers, (x, y) => compareNumbers(x, y));
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
